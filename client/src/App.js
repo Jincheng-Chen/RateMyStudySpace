df9e03f4e1663c37ac6702f9047ce6ae0ec6493b
denpoly <denpoly@gmail.com>
2022-06-07 10:49:58 -0700

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LandingPage from "./screens/landingPage/LandingPage";
 import "./App.css";
 
 function App() {
@@ -10,13 +12,13 @@ function App() {
       <header className="App-header">
         <BrowserRouter>
           <Routes>
-            <Route index element={<div>Landing Page</div>} />
+            <Route index element={<LandingPage></LandingPage>} />
             <Route path="/search" element={<div>Search Page</div>} />
             <Route
               path="/newStudySpace"
               element={<div>New Study Space Page</div>}
             />
-            <Route path="/newReview" element={<div>Review Page</div>} />
+            <Route path="/newReview" element={<div>New Review</div>} />
           </Routes>
         </BrowserRouter>
       </header>
