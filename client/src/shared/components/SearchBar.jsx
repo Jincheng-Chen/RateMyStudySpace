7be9916d3b6dc45d8638a6f274a502f44f197010
denpoly <denpoly@gmail.com>
2022-06-07 17:16:14 -0700

    fixing linter

--- client/src/shared/components/SearchBar.jsx
+++ client/src/shared/components/SearchBar.jsx
@@ -1,17 +1,15 @@
-import { Autocomplete, TextField } from '@mui/material'
-import React from 'react'
+import { Autocomplete, TextField } from "@mui/material";
+import React from "react";
 
-function SearchBar({options, propClass}) {
+function SearchBar({ options, propClass }) {
   return (
     <Autocomplete
-        options = {options}
-        sx = {{width: "40vw", background: "white"}}
-        renderInput={(params) => <TextField {...params} label="Enter City" />}
-        className = {
-            propClass                   }
-    >
-    </Autocomplete>
-  )
+      options={options}
+      sx={{ width: "40vw", background: "white" }}
+      renderInput={(params) => <TextField {...params} label="Enter City" />}
+      className={propClass}
+    ></Autocomplete>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
