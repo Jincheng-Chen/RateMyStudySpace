84c2362f201e8834e04775df743cbd0d543ebba7
aguirre2 <aguirre2@students.cs.ubc.ca>
2022-06-09 17:31:46 -0700

    Initial component setup for SearchPage

---
+++ client/src/screens/searchPage/SearchPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  Box,
+  Typography,
+  Autocomplete,
+  Card,
+  Rating,
+  Grid,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+function SearchPage() {
+  return <div>Search Page</div>;
+}
+
+export default SearchPage;
