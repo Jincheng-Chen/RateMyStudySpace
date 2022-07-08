f3c821ce759f460677539dd1024af0de946e5a08
denpoly <denpoly@gmail.com>
2022-07-08 11:20:20 -0700

    add env variables

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const { PORT } = process.env;
+const DB = process.env.DATABASE;
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -6,7 +9,7 @@ const reviewRouter = require('./routes/reviews');
 const studySpaceRouter = require('./routes/studySpace');
 
 const app = express();
-
+console.log(DB);
 // start the server
 app.use(cors({}));
 
@@ -16,11 +19,11 @@ app.use(bodyParser.json());
 app.use('/review', reviewRouter);
 app.use('/studySpace', studySpaceRouter);
 
-mongoose.connect('mongodb+srv://root:1d6r47fuRdFgYueF@ratemystudyspace.yc6u7ly.mongodb.net/test').then(() => {
+mongoose.connect(DB).then(() => {
   console.log('connected to db');
 });
 
 app.listen(
-  3000,
-  () => console.log('Server listening on port 3000.'),
+  PORT,
+  () => console.log(`Server listening on port ${PORT}.`),
 );
