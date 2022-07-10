73c93e6de80843fb39d52b902e116ce70bbe768c
Jincheng Chen <51464306+Jincheng-Chen@users.noreply.github.com>
2022-07-10 15:35:39 -0700

    Implement PATCH and DELETE for Reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const {
   getReviews,
+  updateReview,
   addNewImage,
   addNewReview,
   deleteReview,
@@ -14,19 +15,22 @@ const {
 // GET all the reviews
 router.get('/', getReviews);
 
+// POST a new Review to the collection
+router.post('/', addNewReview);
+
 // GET reviews by reviewId
 router.get('/:reviewID', getReviewsByReviewId);
+
+// DELETE a specific review by id
+router.delete('/:reviewID', deleteReview);
+
+// Patch a specific review by id
+router.patch('/:reviewID', updateReview);
 
 // Get all the reviews of a specific studySpaceId
 router.get('/studySpace/:studySpaceId', getReviewsByStudySpaceId);
 
-// POST a new Review to the collection
-router.post('/', addNewReview);
-
 // PATCH a new image to the specific review
 router.patch('/addNewImage/:reviewId', addNewImage);
-
-// DELETE a specific review by id
-router.delete('/deleteReview/:reviewId', deleteReview);
 
 module.exports = router;
