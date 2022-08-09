7cf1402842f3821345aeb2898e4ebe79f4b2faa5
emagui <emersonhaguirre@gmail.com>
2022-08-09 16:20:27 -0700

    Removed print statements and comments back-end

--- controllers/studySpaceController.js
+++ controllers/studySpaceController.js
@@ -41,7 +41,6 @@ const getStudySpaceFiltered = (req, res) => {
   ]).then((found) => {
     res.status(200).json(found);
   }).catch((err) => {
-    console.log(err);
     res.status(400).json(err);
   });
 };
@@ -53,7 +52,6 @@ const getStudySpaceByLocation = async (req, res) => {
     const AllstudySpace = await StudySpace.find({ location }).sort({ createdAt: -1 });
     res.status(200).json(AllstudySpace);
   } catch (error) {
-    console.log(error.message);
     res.status(400).json({ error: error.message });
   }
 };
@@ -68,16 +66,13 @@ const getStudySpacesByRadialLocation = async (req, res) => {
           $maxDistance: parseInt(radius * 1000, 10),
           $geometry: {
             type: 'Point',
-            /*eslint-disable */
-          coordinates: [parseFloat(lon, 10), parseFloat(lat, 10)]
-          /* eslint-enable */
+            coordinates: [parseFloat(lon, 10), parseFloat(lat, 10)],
           },
         },
       },
     });
     res.status(200).json(studySpaces);
   } catch (error) {
-    console.log(error.message);
     res.status(400).json({ error: error.message });
   }
 };
@@ -86,7 +81,6 @@ const addNewStudySpace = async (req, res) => {
   try {
     await StudySpace.create({ ...req.body });
     const AllstudySpace = await StudySpace.find({}).sort({ createdAt: -1 });
-    console.log(AllstudySpace[0]);
     res.status(200).json(AllstudySpace[0]);
   } catch (error) {
     res.status(400).json({ error: error.message });
