1964a0a277f4a062f56d9d9d6c8113b17abdf22e
Jincheng Chen <51464306+Jincheng-Chen@users.noreply.github.com>
2022-08-08 17:01:14 -0700

    Ready to bring change to deploy - Style landing page

--- client/src/features/api/studySpaceApiSlice.js
+++ client/src/features/api/studySpaceApiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const studySpaceApiSlice = createApi({
   reducerPath: "studySpaceApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  baseQuery: fetchBaseQuery(),
   tagTypes: ["StudySpace"],
   endpoints: (builder) => ({
     getStudySpace: builder.query({
